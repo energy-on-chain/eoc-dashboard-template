0189cd626cf6239db39bb7c8b804794b815a9f06
hartimat <matthew.t.hartigan@gmail.com>
2022-01-26 23:12:55 -0600

    Implemented navbar for site. Create pages / content next

--- index.py
+++ index.py
@@ -16,10 +16,27 @@ app.layout = html.Div(
         dbc.Row(
             className='navbar',
             children=[
-                dbc.Col(),    # company name
-                dbc.Col(),    # page links
-                dbc.Col(),    # company logo (right justified)
-            ]
+                dbc.Col(
+                    children=[
+                        # html.H2(className='navbar-title-link', children='Energy On Chain   |'),
+                        html.H2(html.A(className='navbar-title-link', children='Energy On Chain   |', href='https://www.google.com')),
+                    ],
+                    width='auto'
+                ),
+                dbc.Col(
+                    children=[
+                        html.A(className='navbar-first-link', children='Page 1', href='https://www.google.com'),
+                        html.A(className='navbar-link', children='Page 2', href='https://www.google.com'),
+                        html.A(className='navbar-link', children='Page 3', href='https://www.google.com'),
+                    ]
+                ), 
+                dbc.Col(
+                    children=[
+                        html.Img(className='navbar-logo', src=app.get_asset_url("full_eoc_logo.jpg"), height=50)
+                    ],
+                )   
+            ],
+            align='center'
         ),
 
         # PAGE CONTENT
@@ -46,9 +63,16 @@ if __name__ == '__main__':
 
 
 # TODO
-# Create navbar
-# Add performance page
-# Add coin comparison page
-# Add revenue / interest page
-# Add other pages...
-# Link to website...
+# Page 1 - Summary
+#   Table showing the coins you hold
+#   Create csv file that holds all this info that can be plugged in
+#   Populate table... bag size, cost basis, gains, losses, ROI, ath drawdown
+# Page 2 - Metrics
+#   Recreate the 7 metrics from my excel sheet here
+# Page 3 - Revenue
+#   Show annualized interest being earned for each coin
+# Page 4 - Coin Comparator
+#   Select any coin you are holding and toggle on BTC / ETH price action
+# Page 5 - Any other custom page you want...
+# Post this on EOC site 
+# Relaunch the EOC site
