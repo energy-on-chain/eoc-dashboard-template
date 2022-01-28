779dd06604aa40c046b2e90020aea96cf9b6aa07
hartimat <matthew.t.hartigan@gmail.com>
2022-01-27 19:53:38 -0600

    Added page_template. Next add basic page content for portfolio project

--- index.py
+++ index.py
@@ -4,10 +4,13 @@ from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 from app import app
 from app import server
+from pages import page_template
 
 
 # LAYOUT
-app.layout = html.Div(
+app.layout = dbc.Container(
+    className='page-container',
+    fluid=True,
     children=[
         # URL
         dcc.Location(id='url', refresh=False),
@@ -18,21 +21,20 @@ app.layout = html.Div(
             children=[
                 dbc.Col(
                     children=[
-                        # html.H2(className='navbar-title-link', children='Energy On Chain   |'),
-                        html.H2(html.A(className='navbar-title-link', children='Energy On Chain   |', href='https://www.google.com')),
+                        html.H2(html.A(className='navbar-title-link', children='Energy On Chain |', href='/')),
                     ],
                     width='auto'
                 ),
                 dbc.Col(
                     children=[
-                        html.A(className='navbar-first-link', children='Page 1', href='https://www.google.com'),
-                        html.A(className='navbar-link', children='Page 2', href='https://www.google.com'),
-                        html.A(className='navbar-link', children='Page 3', href='https://www.google.com'),
+                        html.A(className='navbar-first-link', children='Page 1', href='/page_template'),
+                        html.A(className='navbar-link', children='Page 2', href='/page_template'),
+                        html.A(className='navbar-link', children='Page 3', href='/page_template'),
                     ]
                 ), 
                 dbc.Col(
                     children=[
-                        html.Img(className='navbar-logo', src=app.get_asset_url("full_eoc_logo.jpg"), height=50)
+                        html.Img(className='navbar-logo', src=app.get_asset_url('full_eoc_logo.jpg'), height=50)
                     ],
                 )   
             ],
@@ -40,22 +42,25 @@ app.layout = html.Div(
         ),
 
         # PAGE CONTENT
-        dbc.Row(
+        html.Br(),
+        html.Div(
+            id='page-content',
             className='page-content',
             children=[]
-        ),
+        )
     ]
 )
 
-
 # CALLBACKS
-# @app.callback(Output('page-content', 'children'),
-#               [Input('url', 'pathname')])
-# def update_page(pathname):
-#     if pathname == '/':
-#         return index.layout
-#     else:
-#         return "404 Page Error! Please choose a link"
+@app.callback(Output('page-content', 'children'),
+              [Input('url', 'pathname')])
+def update_page(pathname):
+    if pathname == '/':
+        return index.layout
+    elif pathname == '/page_template':
+        return page_template.layout
+    else:
+        return "404 Page Error! Please choose a link"
 
 
 if __name__ == '__main__':
